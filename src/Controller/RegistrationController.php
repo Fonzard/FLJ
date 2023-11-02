b06ad1cd9e8b89b2d3c6e6ab01b700326cba41ea
Fonzard <francisrouxel2@gmail.com>
2023-11-02 16:10:18 +0100

    📦 NEW : Register mail verification init

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,25 +3,34 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Service\MailerService;
+use Doctrine\ORM\EntityManager;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, MailerService $mailerService, TokenGeneratorInterface $tokenGeneratorInterface): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
+
+            // TOKEN
+            $tokenRegistration = $tokenGeneratorInterface->generateToken();
+
+            // USER
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
@@ -29,9 +38,25 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            // USER TOKEN
+            $user->setTokenRegistration($tokenRegistration); 
+
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
+
+            // MAILER SEND
+            $mailerService->send(
+                $user->getEmail(),
+                'Confirmation du compte utilisateur',
+                'registration_confirmation.html.twig',
+                 [
+                    'user' => $user,
+                    'token' => $tokenRegistration,
+                    'lifeTimeToken' => $user->getTokenRegistrationLifeTime()->format('d-m-Y-H-i-s')
+                 ]
+            );
+            
+            $this->addFlash('sucess', 'Votre compte à bien été créé, veuillez vérifier vos emails pour l\'activer.');
 
             return $this->redirectToRoute('app_login');
         }
@@ -39,5 +64,29 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/verify/{token}/{id<\d+>}', name: 'account_verify', methods:['GET'])]
+    public function verify(string $token, User $user, EntityManagerInterface $em): Response {
+
+        if($user->getTokenRegistration() !== $token){
+            throw new AccessDeniedException();
+        }
+
+        if($user->getTokenRegistration() === null){
+            throw new AccessDeniedException();
+        }
+
+        if(new DateTime('now') > $user->getTokenRegistrationLifeTime()){
+            throw new AccessDeniedException();
+        }
+
+        $user->setIsVerified(true);
+        $user->setTokenRegistration(null);
+        $em->flush();
+
+        $this->addFlash('success', 'Votre compte à bien été activé, vous pouvez maintenant vous connecter.');
+
+        return $this->redirectToRoute('app_login');
     }
 }
